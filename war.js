99f78534c49b500f4bdcd2f18a6b0f9a4ef4a9ed
Sbilavanh <bilavanh.souphalak@gmail.com>
2022-07-08 11:13:01 -0700

    update

--- war.js
+++ war.js
@@ -1,4 +1,4 @@
-function Deck() {
+function Deck() {  //this creates the 52 card deck used to play war
     let deck = [];
     let count = 1;
     for (let i = 1; count <= 4; i++) {
@@ -11,7 +11,7 @@ function Deck() {
     return deck;
 }
 
-function shuffle(array) {
+function shuffle(array) {    // this function is to shuffle the cards randomly to be prepared to be dealt to each player
     let currentIndex = array.length;
 
     while (currentIndex != 0) {
@@ -22,7 +22,7 @@ function shuffle(array) {
     return array;
 }
 
-function player(array) {
+function player(array) {   // this function is used to deal 26 cards to the 2 players in the game
     let deck = [];
     for (let i = 1; i <= 26; i++) {
         deck.push(array.shift());
@@ -36,32 +36,32 @@ function card(array) {
 
 function play_war(arr, array) {
     let card = 26;
-    let nyles_card = 0;
-    let michael_card = 0;
-    let nyles_score = 0;
-    let michael_score = 0;
+    let moch_card = 0;
+    let yu_card = 0;
+    let moch_score = 0;
+    let yu_score = 0;
     while (card != 0) {
-        nyles_card = arr.shift();
-        michael_card = array.shift();
-        if (nyles_card > michael_card) {
-            console.log("nyles won the battle and recieved a point");
-            nyles_score++;
+        moch_card = arr.shift();
+        yu_card = array.shift();
+        if (moch_card > yu_card) {
+            console.log("Moch won the battle and recieved a point");
+            moch_score++;
         }
-        else if (nyles_card < michael_card) {
-            console.log("michael won the battle and recieved a point");
-            michael_score++;
+        else if (moch_card < yu_card) {
+            console.log("Yu won the battle and recieved a point");
+            yu_score++;
         }
         else {
             console.log("both players tied and recieved nothing!");
         }
         card--;
     }
-    console.log("nyles score: " + nyles_score + " michael score: " + michael_score);
-    if (nyles_score > michael_score) {
-        console.log("nyles won the war");
+    console.log("Moch score: " + moch_score + " Yu score: " + yu_score);
+    if (moch_score > yu_score) {
+        console.log("Moch won the war");
     }
-    else if (nyles_score < michael_score) {
-        console.log(" michael won the war");
+    else if (moch_score < yu_score) {
+        console.log(" Yu won the war");
     }
     else {
         console.log("both players tied");
@@ -70,8 +70,8 @@ function play_war(arr, array) {
 }
 
 let deck = (shuffle(Deck()));
-let player_nyles = player(deck);
-let player_michael = player(deck);
-play_war(player_nyles, player_michael);
+let player_moch = player(deck);
+let player_yu = player(deck);
+play_war(player_moch, player_yu);
 
-console.log("nyles score: " + nyles_score + " michael score: " + michael_score);
+console.log("Moch score: " + moch_score + " Yu score: " + yu_score);
