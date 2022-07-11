5cd9f2190935209a2366e8bd2ca806fcae9723c6
Sbilavanh <bilavanh.souphalak@gmail.com>
2022-07-11 08:44:15 -0700

    update

---
+++ unitTest.js
@@ -0,0 +1,10 @@
+let expect = chai.expect;
+
+describe("Function Deck", function () {
+    it('This should create a 52 card deck' function () {
+        let deck = []
+        let start = 1
+        expect(deck).to.be.a("Array");
+        expect(start).to.equal(1);
+    }
+}
